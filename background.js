c5c405cd0d4e4fa9454f89fc77ef16440713aac1
martinowren <martin.owren@gmail.com>
2020-06-05 14:26:03 +0200

    Cleanup of old files

--- background.js
+++ background.js
@@ -1,16 +1,3 @@
-// chrome.storage.local.set({'fikenDarkmode': true}, function() {
-// 	console.log("Value is set to True");
-// });
-// chrome.tabs.onUpdated.addListener((activeTabID, changeInfo, tab) => {
-// 	console.log("On updated");
-// 	console.log(activeTabID);
-// 	console.log(changeInfo);
-// 	console.log(tab);
-// 	chrome.tabs.query
-// 	chrome.tabs.get(activeTabID, (tab) => {
-// 		console.log(tab)
-// 	});
-// });
 chrome.tabs.onUpdated.addListener(() => {
 	chrome.tabs.query({currentWindow: true, active: true}, (tab) => {
 		if(!(tab.length == 0)){
@@ -24,6 +11,3 @@ chrome.tabs.onUpdated.addListener(() => {
 		}
 	})
 })
-// chrome.tabs.getCurrent(function(tab){
-// 	console.log(tab);
-// });
